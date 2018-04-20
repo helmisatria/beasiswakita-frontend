404243f041c46519409bdd4a3ed232a7a48e17f7
Helmi Satria <satriahelmi@gmail.com>
2018-04-21 03:35:24 +0700

    header responsivity done

--- src/components/Header/v2.js
+++ src/components/Header/v2.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Button } from 'semantic-ui-react';
+import { Button, Grid, Icon, Dropdown, Divider } from 'semantic-ui-react';
 
 import Logo from '../../dist/assets/logo@3x.png';
 import FindIcon from '../../dist/assets/search';
 import BookIcon from '../../dist/assets/book-open';
+
+import '../../dist/css/header.css';
 
 export default ({ isExplore }) => (
   <div id="search_header">
@@ -32,19 +34,58 @@ export default ({ isExplore }) => (
           <span className="header_contentSubTitle">Tips & Trik Mencari Beasiswa</span>
         </div>
       </NavLink>
-
-      <Button id="search_headerPostingBtn">Posting Beasiswa</Button>
+      <Grid>
+        <Grid.Column only="tablet computer">
+          <Button id="search_headerPostingBtn">Posting Beasiswa</Button>
+        </Grid.Column>
+      </Grid>
     </div>
     <div className="search_headerCornerContainer">
-      <NavLink
-        to="/login"
-        className="search_headerCornerText"
-      >
-        Login
-      </NavLink>
-      <NavLink to="/daftar">
-        <span className="search_headerCornerText">Daftar</span>
-      </NavLink>
+      <Grid>
+        <Grid.Column only="tablet computer">
+          <NavLink
+            to="/login"
+          >
+            <span className="search_headerCornerText login">Login</span>
+          </NavLink>
+          <NavLink to="/daftar">
+            <span className="search_headerCornerText">Daftar</span>
+          </NavLink>
+        </Grid.Column>
+      </Grid>
     </div>
+    <Grid>
+      <Grid.Column only="mobile" id="hamburgerHeader">
+        <Dropdown
+          trigger={
+            <div className="sidebarContainer hamburger">
+              <Icon name="sidebar" size="large" style={{ margin: 0, color: 'white' }} />
+            </div>
+          }
+          icon={null}
+          direction="left"
+        >
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <Button id="search_headerPostingBtn">Posting Beasiswa</Button>
+            </Dropdown.Item>
+            <Divider fitted />
+            <Dropdown.Item>
+              <NavLink
+                to="/login"
+              >
+                <span>Login</span>
+              </NavLink>
+            </Dropdown.Item>
+            <Dropdown.Item>
+              <NavLink to="/daftar">
+                <span>Daftar</span>
+              </NavLink>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </Grid.Column>
+
+    </Grid>
   </div>
 );
